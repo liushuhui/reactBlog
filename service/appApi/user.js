98e58221e346f9ba68022d2ccd529e928d6054a5
liushuhui <394507099@qq.com>
2020-03-29 20:58:03 +0800

    解决链接数据库报错问题。获取数据库数据
    Signed-off-by: liushuhui <394507099@qq.com>

--- service/appApi/user.js
+++ service/appApi/user.js
@@ -4,9 +4,7 @@ const mongoose = require('mongoose')
 let router = new Router();
 
 router.get('/listTitle', async(ctx) => {
-    console.log(1111)
     const listSchema = mongoose.model('content');
-    console.log('listSchema', listSchema)
     await listSchema.find().then((result) => {
         console.log('result', result)
         ctx.body = {code: 200, data: result}
--- service/index.js
+++ service/index.js
@@ -2,30 +2,33 @@ const Koa = require('koa');
 const app = new Koa();
 const {connect, initSchema} = require('./database/init.js');
 const mongoose = require('mongoose');
-const bodyParser = require('koa-bodyparser')
-const cors = require('koa2-cors')
-const Router = require('koa-router')
+const Router = require('koa-router');
+// const bodyParser = require('koa-bodyparser');
+// const cors = require('koa2-cors');
 
-let user = require('./appApi/user.js')
+// app.use(bodyParser())
+// app.use(cors())
+
+let user = require('./appApi/user.js');
+
 //装载所有子路由
-let router = new Router() 
-router.use('/home', user.routes())
+let router = new Router();
+router.use('/home',user.routes());
 
 //加载路由中间件
+
 app.use(router.routes());
-app.use(router.allowedMethods())
+app.use(router.allowedMethods());
 
-// 链接数据库
 (async() => {
-    await connect();
-     initSchema();
- })()
-
+   await connect();
+    initSchema();
+})()
 
 app.use(async(ctx) => {
-    ctx.body = `<h1>hello world!</h1>`
-})
+    ctx.body = 'hello world!';
+});
 
 app.listen(3100, () => {
-    console.log('[Server] starting at port 3100')
-})
+   console.log( '[Server] starting at port 3100');
+});
